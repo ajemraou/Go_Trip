257fb1c341248d1015507177559f328061a1daa9
github ajemraou <ajemraou@student.1337.ma>
2024-04-23 11:28:43 +0100

    implement additional global middleware for secure http header, data sanitization, and rate limiting

--- app.js
+++ app.js
@@ -4,8 +4,37 @@ const UserRouter = require('./routes/userRoutes');
 const TripRouter = require('./routes/TripRoutes');
 const AppError = require('./utils/appError');
 const ErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
-app.use(express.json());
+const limiter = rateLimit({
+	max: 100,
+	windowMs: 36000,
+	message: 'Too many request from this IP, please try again in a hour!' 
+})
+// Set Security http headers
+app.use(helmet());
+// rate limit 
+app.use('/api', limiter);
+// body pareser
+app.use(express.json({
+	limit: '10kb'
+}));
+// data  sanitization against noSQL query 
+app.use(mongoSanitize());
+// data  sanitization against xss
+app.use(xss());
+
+// Preventing Parameter pollution
+app.use(hpp({
+	whitelist: [
+		'duration', "price"
+	]
+}));
+
 app.use('/api/v1/user', UserRouter);
 app.use('/api/v1/trip', TripRouter);
 
